e5c90913798c33e53505ae6b514c1293f929b47c
parmarKaran01 <82652984+parmarKaran01@users.noreply.github.com>
2021-07-14 11:30:31 +0530

    Update MainPage.js
    Routing

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -3,17 +3,37 @@ import Navbar from './Navbar'
 import CarouselContainer from './carouselContainer'
 import Contact from './Contact'
 import HeroSection from './HeroSection'
-import OilAndGas  from './Industries/oilAndGas'
+import oilAndGas  from './Industries/oilAndGas'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function MainPage() {
     return (
+        <Router>
         <div>
             <Navbar />
-            <HeroSection />
-            <CarouselContainer />
-            <Contact />
-            <OilAndGas />
+            {/* <HeroSection /> */}
+            {/* <CarouselContainer /> */}
+            {/* <Contact /> */}
+            {/* <OilAndGas /> */}
+
+            {/* switch and routing */}
+            <Switch>
+                    <Route path="/" exact>
+                        <HeroSection />
+                        <CarouselContainer />
+                    </Route>
+                    <Route path="/contact" exact>
+                        <Contact />
+                    </Route>
+                    <Route path="/industries" exact>
+                        <oilAndGas />
+                    </Route>
+                    
+                </Switch>
+
+            
         </div>
+        </Router>
     )
 }
 
