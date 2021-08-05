fefda8b4537ed89b30bff9f8375016ce2a58233a
Karan Parmar <mrperfectparmar2001@gmail.com>
2021-08-05 17:41:17 +0530

    Navbar Fixing + Product pages

---
+++ src/Components/Products/PleatedFilters.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PleatedFilters() {
+    return (
+        <div>
+            <h1>Pleated Filter</h1>
+        </div>
+    )
+}
+
+export default PleatedFilters
