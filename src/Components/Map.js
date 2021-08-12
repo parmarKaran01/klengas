a302ea930e7971b4aaf05c215a1a0249f994147e
Karan Parmar <mrperfectparmar2001@gmail.com>
2021-08-12 10:20:14 +0530

    navbar logo + card css fixing

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// import React, { Component } from 'react';
+// import GoogleMapReact from 'google-map-react';
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 19.2307,
-      lng: 72.8567
-    },
-    zoom: 11
-  };
+// class SimpleMap extends Component {
+//   static defaultProps = {
+//     center: {
+//       lat: 19.2307,
+//       lng: 72.8567
+//     },
+//     zoom: 11
+//   };
   
-  render() {
-    return (
-      <div style={{ height: '50vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: API }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          >
-          <AnyReactComponent
-            lat={19.2307}
-            lng={72.8567}
-            text="My Marker"
-            />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
-export default SimpleMap;
+//   render() {
+//     return (
+//       <div style={{ height: '50vh', width: '100%' }}>
+//         <GoogleMapReact
+//           bootstrapURLKeys={{ key: API }}
+//           defaultCenter={this.props.center}
+//           defaultZoom={this.props.zoom}
+//           >
+//           <AnyReactComponent
+//             lat={19.2307}
+//             lng={72.8567}
+//             text="My Marker"
+//             />
+//         </GoogleMapReact>
+//       </div>
+//     );
+//   }
+// }
+// export default SimpleMap;
 
-const API = "AIzaSyC4sWc7sQxxPa51yPWFCmi6QSRWdFxUEr4"
+// const API = "AIzaSyC4sWc7sQxxPa51yPWFCmi6QSRWdFxUEr4"
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import StoreIcon from "@material-ui/icons/Store";
+// import StoreIcon from "@material-ui/icons/Store";
 import { Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
 // import Menu from './Menu'
+import KlengasLogo from '../assets/images/KlengasLogo.png'
 
 //test karan
 import {
   Menu,
   MenuItem,
-  MenuButton
+  MenuButton,
 } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css'
+
 //
 function Navbar() {
   const [showlinks, setShowlinks] = useState(false);
   return (
     <div className="navbar">
       <div className="navbar__leftside">
-        <StoreIcon className="store__logo" />
-        <h3>KLENGAS</h3>
+        {/* <StoreIcon className="store__logo" />
+        <h3>KLENGAS</h3> */}
+        <img src={KlengasLogo} alt="" className="store__logo"  width="275" height="275"/>
       </div>
 
       <div className="navbar__rightside">
@@ -34,13 +37,14 @@ function Navbar() {
           </Link>
           <Link >
               {/* <Menu name = 'Industries' option1="Industries" option2="Oil And Gas" option3="Pharma" option4="Food and Beverages"/> */}
-              <a>
+              
               <Menu menuButton={<MenuButton className="menu__button">Industries</MenuButton>}>
-                  <MenuItem>
+ 
+                  <MenuItem> 
                     <Link to="/industries">Industries</Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link to="/oilandgas">Oil And Gas</Link>
+                  </MenuItem> 
+                  <MenuItem> 
+                    <Link to="/oilandgas">Oil and Gas</Link>
                   </MenuItem>
                   <MenuItem> 
                     <Link to="/pharmaceuticals">Pharmaceuticals</Link>
@@ -58,10 +62,9 @@ function Navbar() {
                     <Link to="/powergeneration">Power Generation</Link>
                   </MenuItem>
               </Menu>
-              </a>
           </Link>
           <Link >
-          <a>
+          
             {/* <Menu name = 'Products' option1="Oil And Gas" option2="Pharma" option3="Food and Beverages"/> */}
             <Menu menuButton={<MenuButton className="menu__button">Products</MenuButton>}>
                   <MenuItem>
@@ -80,7 +83,6 @@ function Navbar() {
                     <Link to="/pleatedfilters">Pleated Filters</Link>
                   </MenuItem>
               </Menu>
-              </a>
           </Link>
           <Link to="/clients">
             <a>Clients</a>
