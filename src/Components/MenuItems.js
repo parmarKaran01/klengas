9d882aed6ee17428a3b90e1b54128d6f9944fbdf
Karan Parmar <mrperfectparmar2001@gmail.com>
2021-08-18 13:56:31 +0530

    navbar fixing

---
+++ src/Components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: 'Oil And Gas',
+    path: '/oilandgas',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Pharmaceuticals',
+    path: '/pharmaceuticals',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Food And Beverages',
+    path: '/food and beverages',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Power Generation',
+    path: '/powergeneration',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Chemicals',
+    path: '/chemicals',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Water Processing',
+    path: '/waterprocessing',
+    cName: 'dropdown-link'
+  }
+];
